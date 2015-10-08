a1fa5f748aa5236146bba68e2f7dc26cf5f610dd
JuHyun Seong <ernestogevar@naver.com>
2015-10-08 18:17:20 +0900

    1.1.7 연습: 뉴튼 법으로 제곱근 찾기 실습

--- chap1/chap1.6.clj
+++ chap1/chap1.6.clj
@@ -114,3 +114,35 @@
   (if (= x 0)
     0
     y))
+
+;; 1.1.7 연습: 뉴튼 법으로 제곱근 찾기
+(defn average[x y]
+  (/ (+ x y) 2)
+)
+
+(defn improve[guess x]
+  (average guess (/ x guess))
+)
+
+(defn good-enough?[guess x]
+  (< (abs (- (square guess) x)) 0.001)
+)
+
+(defn sqrt-iter[guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)
+  )
+)
+
+(defn sqrt[x]
+  (sqrt-iter 1.0 x)
+)
+
+(sqrt 9)
+
+(sqrt (+ 100 37))
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+
+(square (sqrt 1000))
